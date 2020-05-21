2b2054d27a00ebcd53cdfc47f4d20502ed20a452
Yury Ushchenchyk <yurik.uschen@gmail.com>
2020-05-21 13:16:33 +0300

    init

---
+++ app/src/main/java/com/example/cube_with_pics/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.cube_with_pics;
+
+import android.graphics.PixelFormat;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.RelativeLayout;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+    public GLSurfaceView mGLSurfaceView;
+    float mPreviousX,mPreviousY;
+    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+    MyGLRender mRenderer;
+    RelativeLayout cubelayout;
+
+    @Override    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cubelayout=(RelativeLayout)findViewById(R.id.cude_layout);
+        mGLSurfaceView = new GLSurfaceView(this); // Allocate a GLSurfaceView
+        mRenderer=new MyGLRender(this);
+
+        //make cube transparent
+        mGLSurfaceView.setZOrderOnTop(true);
+        mGLSurfaceView.setEGLConfigChooser( 8, 8, 8, 8, 16, 0 );
+        mGLSurfaceView.getHolder().setFormat(PixelFormat.RGBA_8888);
+        mGLSurfaceView.setRenderer(mRenderer);
+        mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        cubelayout.addView(mGLSurfaceView);
+    }
+
+    @Override    public boolean onTouchEvent(MotionEvent e) {
+        float x = e.getX();
+        float y = e.getY();
+
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                float dx = x - mPreviousX;
+                float dy = y - mPreviousY;
+                mRenderer.mAngleX += dx * TOUCH_SCALE_FACTOR;
+                mRenderer.mAngleY += dy * TOUCH_SCALE_FACTOR;
+                mGLSurfaceView.requestRender();
+                break;
+        }
+        mPreviousX = x;
+        mPreviousY = y;
+        return true;
+    }
+}
